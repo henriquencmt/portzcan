328a5c1cdfa5de71506b6042a32bfccec29de2ea
Henrique Santos <henriquencmt@gmail.com>
2024-04-09 04:37:51 -0300

    initial scanner with hardcoded ports

---
+++ src/main.zig
@@ -0,0 +1,75 @@
+const std = @import("std");
+const os = std.os;
+const posix = std.posix;
+
+pub fn print(comptime message: []const u8, args: anytype) !void {
+    const stdout_file = std.io.getStdOut().writer();
+    var bw = std.io.bufferedWriter(stdout_file);
+    const stdout = bw.writer();
+
+    try stdout.print(message, args);
+
+    try bw.flush();
+}
+
+pub fn connect(target: []u8, port: u16) !void {
+    const sockfd = try posix.socket(posix.AF.INET, posix.SOCK.STREAM, 0);
+    errdefer posix.close(sockfd);
+
+    _ = try posix.fcntl(sockfd, posix.F.SETFL, os.linux.SOCK.NONBLOCK);
+
+    const address = try std.net.Address.parseIp(target, port);
+    if (posix.connect(sockfd, &address.any, address.getOsSockLen())) |_| {
+        try print("1: port {} is open\n", .{port});
+    } else |err| switch (err) {
+        error.ConnectionRefused => try print("port {} is closed\n", .{port}),
+        error.WouldBlock => {
+            const epollfd = try posix.epoll_create1(0);
+            var ev: os.linux.epoll_event = .{
+                .events = os.linux.EPOLL.OUT,
+                .data = .{ .fd = sockfd },
+            };
+            _ = try posix.epoll_ctl(epollfd, os.linux.EPOLL.CTL_ADD, sockfd, &ev);
+
+            var events: [1]os.linux.epoll_event = undefined;
+            const nfds = posix.epoll_wait(epollfd, &events, 1000);
+            switch (nfds) {
+                0 => try print("socket timeout trying to connect to port {}\n", .{port}),
+                1 => {
+                    var so_error: i32 = undefined;
+                    var size: u32 = @sizeOf(u32);
+                    const rc = os.linux.getsockopt(sockfd, posix.SOL.SOCKET, posix.SO.ERROR, @as([*]u8, @ptrCast(&so_error)), &size);
+                    switch (rc) {
+                        0 => try print("2: port {} is open\n", .{port}),
+                        else => try print("socket not connected trying to connect to port {}\n", .{port}),
+                    }
+                },
+                else => try print("epoll_wait() failure trying to connect to port {}\n", .{port}),
+            }
+        },
+        else => try print("failure trying to connect to port {}. error: {any}\n", .{ port, err }),
+    }
+}
+
+pub fn main() !void {
+    const args = try std.process.argsAlloc(std.heap.page_allocator);
+    defer std.process.argsFree(std.heap.page_allocator, args);
+
+    if (args.len < 2) {
+        std.debug.print("Usage: {s} <address>\n", .{args[0]});
+        std.posix.exit(1);
+    }
+
+    const target = args[1];
+    const ports = [_]u16{ 22, 80, 443, 8080, 12017, 5432, 8081 };
+
+    var thr: [ports.len]std.Thread = undefined;
+
+    for (&thr, 0..) |*item, i| {
+        item.* = try std.Thread.spawn(.{}, connect, .{ target, ports[i] });
+    }
+
+    for (thr) |t| {
+        t.join();
+    }
+}

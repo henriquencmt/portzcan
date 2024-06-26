faa2b574d7b13ceca18476f6976ee2cbaa706bb5
Henrique Santos <henriquencmt@gmail.com>
2024-06-25 23:16:24 -0300

    generate docs on build

--- lib/portzcan.zig
+++ lib/portzcan.zig
@@ -1,2 +1,7 @@
 pub const TcpConnectScanner = @import("TcpConnectScanner.zig");
 pub const TcpSynScanner = @import("tcp_syn.zig").TcpSynScanner;
+
+test "portzcan" {
+    _ = TcpSynScanner;
+    _ = TcpConnectScanner;
+}
